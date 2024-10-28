5a683209b9b7f499608cd38c81e657af38ffb9eb
Sharmin Jahan <sjahan@Zipcoders-MacBook-Pro.local>
2024-10-28 17:23:35 -0400

    working

--- src/java/WriteLoops.java
+++ src/java/WriteLoops.java
@@ -14,10 +14,12 @@ public class WriteLoops {
 
     public int oneToFive() {
         int w = 0;
+        for (int i = 1; i<=5; i++){
+            w = w +1;
+        }
 
         // Write a FOR loop that counts from 1 to 10.
             // calling
-            w = w + 1;
             // each time through the loop
 
         // this will tell the test how many times the loop executed.
@@ -26,10 +28,12 @@ public class WriteLoops {
 
     public int oneToTen() {
         int w = 0;
+        for (int i = 1; i<=10; i++){
+            w = w +1;
+        }
 
         // Write a FOR loop that counts from 1 to 10.
         // calling
-        w = w + 1;
         // each time through the loop
         
         return w;
@@ -37,10 +41,12 @@ public class WriteLoops {
 
     public int startAtTwentyOne() {
         int w = 0;
+        for (int i = 21; i<=31; i++){
+            w = w +1;
+        }
 
         // Write a FOR loop that makes 10 iterations, start at 21.
         // calling
-        w = w + 1;
         // each time through the loop
         
         return w;
@@ -48,10 +54,12 @@ public class WriteLoops {
 
     public int countDown() {
         int w = 0;
+        for (int i = 100; i>=0; i--){
+            w = w +1;
+        }
 
         // Write a FOR loop that counts down from 100 to 0.
         // calling
-        w = w + 1;
         // each time through the loop
         
         return w;
@@ -59,20 +67,24 @@ public class WriteLoops {
 
     public int byTwoTo32() {
         int w = 0;
+        for (int i = 0; i<= 32 / 2; i++){
+            w = w +1;
+        }
 
         // Write a FOR loop from 0 to 32 by 2s.
         // calling
-        w = w + 1;
         // each time through the loop
         return w;
     }
 
     public int countDownFrom5000() {
         int w = 0;
+        for (int i = 0; i<= 5000/11; i++){
+            w = w +1;
+        }
 
         // Write a FOR loop from 1 to less than 5001 by 11s.
         // calling
-        w = w + 1;
         // each time through the loop
         
         return w;
@@ -80,30 +92,40 @@ public class WriteLoops {
 
     public int nestedFors() {
         int w = 0;
+        for (int i = 0; i < 20; i++){
+            for (int a = 0; a<=4; a++){
+
+            w = w +1;
+        }
 
         // Write a nested FOR loop(s), where one counts from
         // 0 to less than 20 and the inner one counts from 0 to 4
                 // calling
-                w = w + 1;
                 // each time through the inner loop
 
         return w;
     }
 
-    public int helloZipCode() {
-        int w = 0;
+    public int helloZipcode() {
+            int w = 0;
 
-        // Write a FOR loop that counts from 5 to 105. Put an IF
-        // statement inside the loop that checks the
-        // loop index counter and if it’s greater than 51,
-        // prints “Hello Zipcode” instead of the statement w = w + 1;
-
-                // calling
+            for (int i = 5; i <= 105; i++) {
+            if (i > 51) {
+                System.out.println("Hello Zipcode");
+            } else {
                 w = w + 1;
-            // each time through the inner loop
-        
+            }
+        }
         return w;
+
     }
+
+
+
+
+
+        
+
 
     public void simpleLoops() {
         int i = 0;
